fa97722d7d7fb8ee4c12e9bf72e015482e040774
Tony Hoyle <tony@hoyle.me.uk>
2023-07-22 22:29:52 +0100

    Go back to working formula

--- lib/gps.dart
+++ lib/gps.dart
@@ -64,18 +64,20 @@ class GpsData {
     // Bearing
     // This is v2 with more complex maths, because the straight distance calc
     // was frequently a couple of degrees out, and that's no good for a GPS.
+    //
+    // At low speeds <1m/s this is probably wrong due to jitter in the GPS signal.
+    // Possibly calculate second only if speed is faster
     final ns = sin(lon1 - lon2);
     final ew = cos(lat1) * tan(lat2) -  sin(lat1) * cos(lon1 - lon2);
     calculatedBearing = _radToDeg(atan2(ns, ew));
 
     // Straight line distance
     // There are about a dozen variants of this, all produce slightly
-    // different results, plus there's the pythagorean approximation..
+    // different results, some don't even work...
     const earthRadius = 6378137.0;
     final dLat = sin(lat2 - lat1) / 2;
     final dLon = sin(lon2 - lon1) / 2;
-    final a = (dLat*dLat) * (dLon*dLon) + cos(lat1) * cos(lat2);
-    final flatDistance = 2000 * earthRadius * atan2(sqrt(a), sqrt(1-a));
+    final flatDistance = 2000 * earthRadius * asin(sqrt((dLat*dLat) + cos(lat1) * cos(lat2) * (dLon*dLon)));
 
     // Take into account rise, if available.. this matters on steep hills
     final rise = altitude - lastAltitude;
